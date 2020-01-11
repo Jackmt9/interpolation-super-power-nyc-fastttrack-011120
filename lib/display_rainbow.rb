b206da43c0581c01ed547caa3635aba7563d7649
Jack T <jackmt9@gmail.com>
2020-01-11 16:48:52 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -6,5 +6,5 @@ def display_rainbow(colorArray)
   print "G: #{colorArray[3]}, "
   print "B: #{colorArray[4]}, "
   print "I: #{colorArray[5]}, "
-  print "V: #{colorArray[6]}"
+  puts "V: #{colorArray[6]}"
 end
